2ff5f7b18040f5bdb9b29d918e31173637796d02
Rahul Jadhav <138716867+Rahul7434@users.noreply.github.com>
2023-07-31 01:52:29 +0530

    Add files via upload

---
+++ Clear_list_Using_clear_fun.py
@@ -0,0 +1,12 @@
+# Using Clear FUnction
+l=[10,20,30,50,60]
+print("List",l)
+print("List Clear",l.clear())
+
+
+# Without Using Clear Function
+
+l1=[1,2,3]
+print("Before Clear List:-",l1)
+l1 *=0
+print("Clear List:-",l1)
